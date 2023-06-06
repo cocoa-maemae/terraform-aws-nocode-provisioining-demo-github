8b13791aa7a9d2cb67c528951f39d26733e88b8b
tomoki.maeda <you@example.com>
2023-06-06 17:36:37 +0900

    fix topic

--- main.tf
+++ main.tf
@@ -2,14 +2,12 @@ resource "aws_sns_topic" "example_topic" {
   name = "example-sns-topic"
 }
 
-resource "aws_sns_topic_subscription" "higa_updates_email_target" {
-  topic_arn = "arn:aws:sns:ap-southeast-1:861023892450:example-sns-topic"
+resource "aws_sns_topic_subscription" "example_email_target" {
+  topic_arn = aws_sns_topic.example_topic.arn
   protocol  = "email"
   endpoint  = "tomoki.maeda@hashicorp.com"
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
   region     = "ap-northeast-1"
 }
